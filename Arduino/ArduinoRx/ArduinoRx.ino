347aa45925eb5ae047e471f59fa4e4e5f7c41bc0
SiennaRose <siennarose.sacramento@gmail.com>
2024-04-15 19:02:31 -0700

    added lower and upper bounds to the arduino Rx serial plotter graph

---
+++ Arduino/ArduinoRx/ArduinoRx.ino
@@ -0,0 +1,23 @@
+int sensorOutput = 4; // The analog pin connected to the sensor
+int THRESHOLD = 1;
+int graphUpperLimit = 500;
+int graphLowerLimit = 0;
+
+void setup()
+{
+  Serial.begin(9600); // Serial init
+}
+
+void loop()
+{
+  int value = analogRead(sensorOutput); // Read analog voltage from sensor
+
+  if (value >= THRESHOLD) // Check voltage level from sensor
+  {
+    Serial.print(graphLowerLimit); // To freeze the lower limit
+    Serial.print(" ");
+    Serial.print(graphUpperLimit); // To freeze the upper limit
+    Serial.print(" ");
+    Serial.println(value); // To send all three 'data' points to the plotter
+  }
+}
