fca8b27cb4244361b7a8e0ceb618e673e5ab96e4
SiennaRose <siennarose.sacramento@gmail.com>
2024-05-02 20:42:34 -0700

    started work on the user interface code

--- Arduino/ArduinoRx/ArduinoRx.ino
+++ Arduino/ArduinoRx/ArduinoRx.ino
@@ -1,11 +1,16 @@
 int sensorOutput = 4; // The analog pin connected to the sensor
 int THRESHOLD = 1;
-int graphUpperLimit = 500;
+int graphUpperLimit = 10;
 int graphLowerLimit = 0;
 
 void setup()
 {
   Serial.begin(9600); // Serial init
+
+    Serial.print(graphLowerLimit); // To freeze the lower limit
+    Serial.print(" ");
+    Serial.print(graphUpperLimit); // To freeze the upper limit
+    Serial.print(" ");
 }
 
 void loop()
