3bbd0ab43b83052430352ed2be5e7b893b73abfa
Akshaya <akbhat@ucsd.edu>
2024-05-30 21:14:32 -0700

    Cleaned up transmitter code
    Signed-off-by: Akshaya <akbhat@ucsd.edu>

--- PYNQ/src/tx_src/transmitter.cpp
+++ PYNQ/src/tx_src/transmitter.cpp
@@ -13,6 +13,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <cstring>
 #include <string.h>
 #include <algorithm>
 #include <algorithm>
@@ -21,11 +22,12 @@
 
 using namespace std;
 
-
+#define DEBUG 0
 //make sizes dynamic
 //make the input data 128
 //optimizations, bitstream, and connect to DAC
 
+extern "C" {
 string string2Ascii(std::string str, int len)
 {
 	int val;
@@ -41,7 +43,9 @@ string string2Ascii(std::string str, int len)
 void transmitter (string input, float* output_i, int len)
 {
 
+	cout << input << endl;
   	string s = string2Ascii(input, len);
+
 	cout << s << endl;
 	
 	int n = len * 8;
@@ -50,24 +54,56 @@ void transmitter (string input, float* output_i, int len)
  
 	cout << "size of code: " << n << endl;
 
-	for (int i = 0; i < n; i = i+2)
-	{
-	  //cout << i << endl;
-	  input_i[i/2] = atoi(&s[i]); 
-	  input_q[i/2] = atoi(&s[i+1]); 
-  	}
+	char* char_array = new char[n + 1];
+	int input_bits[n];
+        strcpy(char_array, s.c_str());
+	for (int i=0; i<n; i++) {
+	    input_bits[i] = char_array[i] - '0';
+	}
+	for (int i=0; i<n; i=i+2) {
+		input_i[i/2] = input_bits[i];
+		input_q[i/2] = input_bits[i+1];
+        }
+
+#ifdef DEBUG
+	//Check our input bits
+	cout << "Input Bits" << endl;
+	for (int i=0; i<n; i++) {
+		cout << input_bits[i] << endl;
+	}
+#endif
+	// we do now need to bit pack this
+	int bytes_i[len/2];
+	int bytes_q[len/2];
+	for (int i=0; i<len/2; i++) {
+		bytes_i[i] = 0;
+		bytes_q[i] = 0;
+		for (int j=0; j<8; j++) {
+			bytes_i[i] += input_i[i*8+j] << (7-j);
+			bytes_q[i] += input_q[i*8+j] << (7-j);
+		}
+        }
+	//cout << "after bit packing" << endl;
+	//for (int i=0; i<len/2; i++) {
+	//	cout << "i: " << bytes_i[i] << ", q: " << bytes_q[i] << endl;
+	//}
+
 	/**
 	 * Scrambler
 	 * xor with PN gen sequence LUT
 	*/
 
 	int NHalf = n/2;
-	int N = (n * 2) + 12; // The encoder doubles the size and convolutional encoder is zero tailed instead of tail biting so it adds 12 tail bits at thencend.
-	data_t scrambledDataI[NHalf], scrambledDataQ[NHalf];
-	for (int i = 0; i < NHalf; i++) {
-		scrambledDataI[i] = input_i[i] ^ pnGenSequence[i];
-		scrambledDataQ[i] = input_q[i] ^ pnGenSequence[i];
+	int N = n + 12; // The encoder doubles the size and convolutional encoder is zero tailed instead of tail biting so it adds 12 tail bits at thencend.
+	float scrambledDataI[len/2], scrambledDataQ[len/2];
+	for (int i = 0; i < len/2; i++) {
+		scrambledDataI[i] = bytes_i[i] ^ pnGenSequence[i];
+		scrambledDataQ[i] = bytes_q[i] ^ pnGenSequence[i];
 	}
+	//cout << "scrambling results" << endl;
+	//for (int i=0; i<len/2; i++) {
+	//	cout << "i: " << scrambledDataI[i] << ", q: " << scrambledDataQ[i] << endl;
+	//}
 
 	/**
 	 * Encoder
@@ -83,7 +119,7 @@ void transmitter (string input, float* output_i, int len)
 	const int16_t soft_decision_high = +127;
 	const int16_t soft_decision_low  = -127;
 
-	const size_t total_input_bytes = len;
+	const size_t total_input_bytes = len/2;
 	const size_t total_input_bits = total_input_bytes*8u; //128
 	const size_t noise_level = 0;
 	auto enc = ConvolutionalEncoder_Lookup(K, R, G);
@@ -93,10 +129,10 @@ void transmitter (string input, float* output_i, int len)
 	const size_t total_data_bits = total_input_bytes*8;
 	const size_t total_bits = total_data_bits + total_tail_bits;
 	const size_t total_symbols = total_bits * R;
-	data_t encodedDataI[total_symbols];
-	data_t encodedDataQ[total_symbols];
+	float encodedDataI[total_symbols];
+	float encodedDataQ[total_symbols];
 	output_symbols.resize(total_symbols);
-   
+
 	encode_data(&enc, tx_input_bytes.data(), tx_input_bytes.size(),
 		output_symbols.data(), output_symbols.size(),
 		soft_decision_high, soft_decision_low);
@@ -112,6 +148,12 @@ void transmitter (string input, float* output_i, int len)
 	tx_input_bytesQ.resize(total_input_bytes);
 	output_symbolsQ.resize(total_symbols);
 
+	// debug
+	//cout << "Q input bytes" << endl;
+	//for (int i=0; i<total_input_bytes; i++) {
+	//	cout << static_cast<int>(tx_input_bytesQ[i]) << endl;
+        //}
+
 	//    generate_random_bytes(tx_input_bytes.data(), tx_input_bytes.size());
 	encode_data(&enc, tx_input_bytesQ.data(), tx_input_bytesQ.size(),
 			output_symbolsQ.data(), output_symbolsQ.size(),
@@ -123,15 +165,15 @@ void transmitter (string input, float* output_i, int len)
 		           for (int i = 0; i < output_symbolsQ.size(); i++) {
 				   encodedDataQ[i] = output_symbolsQ[i];
 		           }
-		               
+
 			   //cout << "copied encodedQ\n";
 
 		/**
 		 * Symbol Mapping
 		 * 	QPSK
 		 */
-		data_t qpskDataI[N];
-		data_t qpskDataQ[N];
+		float qpskDataI[N];
+		float qpskDataQ[N];
 
 		for (int i = 0; i < N; i++) {
 			if (encodedDataI[i] < 0) {
@@ -153,18 +195,14 @@ void transmitter (string input, float* output_i, int len)
 		/**
 		 * Golay Preamble
 		 */
-//		std::complex<double> raw_symbols[100];
-//
-//			for (int i = 0; i < 100; i++) {
-//				raw_symbols[i] = std::complex<double>(qpskDataI[i], qpskDataQ[i]);
-//			}
+
 		//Theres a weird zero somewhere
 		//put in zeros in the middle
-		double preamble_qpsk[preambleLen];
+		float preamble_qpsk[preambleLen];
 		for (int i = 0; i < preambleLenHalf; ++i) {
 			preamble_qpsk[i] = Ga[i]*1.414;
 		}
-		//cout << "golay\n";
+
 		int x = 0;
 		for (int i = preambleLenHalf; i < preambleLen; ++i) {
 			preamble_qpsk[i] = Gb[x]*1.414;
@@ -173,8 +211,8 @@ void transmitter (string input, float* output_i, int len)
 
 		//cout << "golay2\n";
 
-		double symbolsI[N+preambleLen+32];
-		double symbolsQ[N+preambleLen+32];
+		float symbolsI[N+preambleLen+32];
+		float symbolsQ[N+preambleLen+32];
 			for (int i = 0; i < preambleLen; ++i) {
 				symbolsI[i] = preamble_qpsk[i];
 				symbolsQ[i] = 0.0;
@@ -193,14 +231,20 @@ void transmitter (string input, float* output_i, int len)
 				x++;
 			}
 
-			//cout << "symbolsI\n";
-					for (int i = 0; i < 140; i++) {
-							            	   //cout << symbolsI[i] << endl;
-							               }
-					//cout << "symbolsQ\n";
-										for (int i = 0; i < 140; i++) {
-												            	   //cout << symbolsQ[i] << endl;
-												               }
+		// Write the symbols to an output file
+		cout << "tx symbols:" << endl;
+        FILE *fpsense = fopen("/home/xilinx/tx_symbols.bin","wb");
+        for (int i = 0; i < 236; ++i) {
+            float realPart = symbolsI[i];
+			float imagPart = symbolsQ[i];
+            fwrite(&(realPart), sizeof(float),1, fpsense);
+			fwrite(&(imagPart), sizeof(float),1, fpsense);
+			//cout << "i: " <<realPart << ", q: " << imagPart << endl;
+        }
+		cout << endl;
+
+		//cout << endl;
+		fclose(fpsense);
 		/**
 		 * Pulse Shaping
 		 * 	SRRC Filter
@@ -208,13 +252,16 @@ void transmitter (string input, float* output_i, int len)
 		//Upsample
 
 		int upsampleSize = oversample*(N+preambleLen+32);
+		cout << "len: " << len << endl;
+		cout << "N: " << N << endl;
+		cout << "oversample: " << oversample << endl;
+		cout << "number of bits to upsample: " << N+preambleLen+32 << endl;
 		cout << "upsamplesize: " << upsampleSize << endl;
-		double dataUpsampledI[upsampleSize];
-		double dataUpsampledQ[upsampleSize];
+		float dataUpsampledI[upsampleSize];
+		float dataUpsampledQ[upsampleSize];
 		std::fill(dataUpsampledI, dataUpsampledI + upsampleSize, 0.0);
 		std::fill(dataUpsampledQ, dataUpsampledQ + upsampleSize, 0.0);
 
-		//cout << "pulse shaped start " << endl;
 			int j = 0;
 			for (int i = 0; i < upsampleSize; i+=oversample) {
 				dataUpsampledI[i] = symbolsI[j];
@@ -222,56 +269,65 @@ void transmitter (string input, float* output_i, int len)
 				j++;
 			}
 
-		//cout << "pulse shaped start 1" << endl;
 
-
-		double dataPulseShapedI[upsampleSize];
-		double dataPulseShapedQ[upsampleSize];
+		float dataPulseShapedI[upsampleSize];
+		float dataPulseShapedQ[upsampleSize];
 			//Convolution
 			for (int i = 0; i < upsampleSize; i++) {
 				dataPulseShapedI[i] = 0.0;
 				dataPulseShapedQ[i] = 0.0;
-				for (int j = 0; j < 193; j++) {
+				for (int j = 0; j < 97; j++) {
 					dataPulseShapedI[i] += dataUpsampledI[i-j] * h[j];
 					dataPulseShapedQ[i] += dataUpsampledQ[i-j] * h[j];
 				}
 			}
 		//cout << "pulse shaped " << endl;
-		FILE *fp2 = fopen("/home/xilinx/capstone/armcode/data/outPulseShapred_HLS_new.bin","wb");
-			for (int i = 0; i < (upsampleSize); ++i) {
-				//cout << i << " " << dataPulseShapedI[i] << " " << dataPulseShapedQ[i] << endl;
-				float realPart = dataPulseShapedI[i];
-				float imagPart = dataPulseShapedQ[i];
-				fwrite(&(realPart), sizeof(float),1, fp2);
-				fwrite(&(imagPart), sizeof(float),1, fp2);
-			}
-
+		FILE *fp2 = fopen("/home/xilinx/tx_pulse_shaped.txt","w");
+		for (int i = 0; i < (upsampleSize); ++i) {
+			float realPart = dataPulseShapedI[i];
+			fwrite(&(realPart), sizeof(float),1, fp2);
+			//cout << realPart << "," << endl;
+		}
+		//cout << endl;
+		for (int i = 0; i < (upsampleSize); ++i) {
+			float imagPart = dataPulseShapedQ[i];
+			fwrite(&(imagPart), sizeof(float),1, fp2);
+			//cout << imagPart << "," << endl;
+		}
 		fclose(fp2);
+		//cout << endl;
 
 		/**
 		 * Modulation
 		 */
 
 		cout << "modulation 1 " << upsampleSize << endl;
-		double theta;
+		int index = 0;
 		for (int i = 0; i < upsampleSize; i++) {
-			double t = static_cast<double>(i) / fs;
-			theta = fc * t;
-			int index = static_cast<int>(theta*(32.0)) % 32; //size of cos/sin LUT -1
-			double cos = cos_coefficients_table[index];
-			double sin = -1.0 * sin_coefficients_table[index];
-			//cout << " i " << i << " cos " << cos << endl;
+			float cos = cos_coefficients_table[index];
+			float sin = -1.0 * sin_coefficients_table[index];
 
-			double modI = dataPulseShapedI[i] * cos - dataPulseShapedQ[i] * sin;
-			double modQ = dataPulseShapedI[i] * sin + dataPulseShapedQ[i] * cos;
+			float modI = dataPulseShapedI[i] * cos - dataPulseShapedQ[i] * sin;
+			float modQ = dataPulseShapedI[i] * sin + dataPulseShapedQ[i] * cos;
 
 			output_i[i] =  modI + modQ;
+			//output_i[i] =  modI;
+			index++;
+			if (index >= 23) {
+				index = 0;
+			}
 		}
 
-		cout << "output_i\n";
-							for (int i = 0; i < 140; i++) {
-									            	   //cout << output_i[i] << endl;
-									               }
+                FILE *fp3 = fopen("/home/xilinx/tx_modulated_output.txt","w");
+				//cout << "output" << endl;
+                for (int i = 0; i < (upsampleSize); ++i) {
+					float out_sample = output_i[i];
+                    fwrite(&(out_sample), sizeof(float),1, fp3);
+					//cout << out_sample << "," << endl;
+                }
+
+                fclose(fp3);
+
 
 
 
@@ -286,7 +342,7 @@ void transmitter (string input, float* output_i, int len)
 //	}
 
 }
-
+}
 
 /**
  * Convolutional Encoder
